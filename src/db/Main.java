1294e3476e15f543bcbc7fd88c4ddd1020e4ce6e
przytula94 <przytula94@gmail.com>
2018-05-05 13:23:10 +0200

    ok

--- src/db/Main.java
+++ src/db/Main.java
@@ -8,7 +8,7 @@ public class Main {
         // Open a database connection
         // (create a new database if it doesn't exist yet):
         EntityManagerFactory emf =
-                Persistence.createEntityManagerFactory("$objectdb/db/points.odb");
+                Persistence.createEntityManagerFactory("$objectdb/db/komis.odb");
         EntityManager em = emf.createEntityManager();
 
         // Close the database connection:
