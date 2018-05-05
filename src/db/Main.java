b343655768a11163dd19da2b09bd8390d1a4d994
przytula94 <przytula94@gmail.com>
2018-05-05 13:19:13 +0200

    Initial commit

---
+++ src/db/Main.java
@@ -0,0 +1,20 @@
+package db;
+
+import javax.persistence.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Open a database connection
+        // (create a new database if it doesn't exist yet):
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("$objectdb/db/points.odb");
+        EntityManager em = emf.createEntityManager();
+
+       
+
+        // Close the database connection:
+        em.close();
+        emf.close();
+    }
+}
